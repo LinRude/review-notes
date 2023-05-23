edf12ce744c099cd4e1f2854e33d87b2de8ec404
林如德 <1320002856@qq.com>
2023-05-23 10:40:20 +0800

    加了个对象

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,17 @@
-console.log(1)
+var yuqian = {
+    name : 'lrd',
+    age : 22,
+    isMale : true,
+    job: 'sdss',
+    mobile: '135649874154',
+    nickname: 'ssss',
+    address:{
+        city: '深圳',
+        road: '龙海家园'
+    },
+    parent:{
+        name: 'sdsd',
+        job
+    }
+}
+// 基础类型：string,number,boolean,undefined,null,Symbol,bigInt
