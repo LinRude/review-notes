79d7322350a445feac79205b6eefaf569013534d
林如德 <1320002856@qq.com>
2023-05-23 10:30:51 +0800

    feat-v2-init

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+var yuqian = {
+    name : 'lrd',
+    age : 22,
+    isMale : true,
+    job: 'sdss',
+    mobile: '135649874154',
+    nickname: 'ssss',
+    address:{
+        city: '深圳',
+        road: '龙海家园'
+    },
+    parent:{
+        name: 'sdsd',
+        job
+    }
+}
+// 基础类型：string,number,boolean,undefined,null,Symbol,bigInt
